4d300b461a668569d232b3759e2a0db90f75dd34
nainpedroza <nain.pedroza@gmail.com>
2025-06-08 12:48:21 -0600

    Adding files

---
+++ UTEP_workshop_KAJ/FLO-LDA/F+/alan/run-libxc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+#
+#SBATCH --time=00:25:00
+#SBATCH --nodes=1                # number of different nodes - could be an exact number or a range of nodes (same as -N)
+#SBATCH --ntasks=4                # number of tasks - how many tasks (nodes) that you require (same as -n)
+#SBATCH --mem-per-cpu=2G            # memory required per allocated CPU (or core) - amount of memory (in bytes)
+#SBATCH --job-name F- # you can give your job a name for easier identification (same as -J)
+
+#  The executable used below is from a version that implements symmetrized FOD optimization (using trivial symmetry)
+
+i=1
+l=2
+while [ $i -lt $l ]
+do
+echo $i
+# cp start/FRMORB .
+# cp start/RUNS .
+    srun -n 4 /mnt/home/jacks1kaCMICH/PublicRelease_libxc/flosic/nrlmol_exe >&print 
+ i=$[$i+1]
+done
+# ~/bin/cleanit
+#
---
+++ UTEP_workshop_KAJ/LDA/h2oF/alan/run-dft-relax.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --login
+#
+#SBATCH --time=00:25:00
+#SBATCH --nodes=1                # number of different nodes - could be an exact number or a range of nodes (same as -N)
+#SBATCH --ntasks=5                # number of tasks - how many tasks (nodes) that you require (same as -n)
+#SBATCH --mem-per-cpu=2G            # memory required per allocated CPU (or core) - amount of memory (in bytes)
+#SBATCH --job-name h2oF  # you can give your job a name for easier identification (same as -J)
+
+#  The executable used below is from a version that implements symmetrized FOD optimization (using trivial symmetry)
+
+i=1
+l=21
+while [ $i -lt $l ]
+do
+echo $i
+# cp start/FRMORB .
+# cp start/RUNS .
+    srun -n 5 /mnt/home/jacks1kaCMICH/PublicRelease_libxc/flosic/nrlmol_exe >&print 
+    cat FRCOUT.G0 >> HISTORY
+ i=$[$i+1]
+done
+~/bin/cleanit
+#
